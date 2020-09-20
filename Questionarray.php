b2f7cdb924f8a4e35588bebbd44b1d548d455bc8
amit-420 <amitsawant2424@gmail.com>
2020-09-20 22:56:35 +0530

    added questionarray and tried updating and adding choosen option but relational problem has arised yet to resolve

---
+++ Questionarray.php
@@ -0,0 +1,11 @@
+<?php 
+        $questions = array(
+            array("Q1","orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ?","male","female","Other"),
+            array("Q2","orem ipsum dolor sit amet, incididunt ut labore et dolore magna aliqua. ?","male","female","Other"),
+            array("Q3","orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ?","Male","Female","Other"),
+        )
+
+
+
+
+?>
--- portal.php
+++ portal.php
@@ -1,12 +1,33 @@
 <?php 
       session_start();
-	include('config/db_connect.php');
-
-		if(isset($_POST['submit'])){
+      include('config/db_connect.php');
+      include('Questionarray.php');
+      foreach($questions as $question) {
+            if(isset($_POST[$question[0]])){
                   echo $_POST['gender'];
-
-
+                  #For adding table $sql = "CREATE TABLE test (
+                              // PersonID int,
+                              // LastName varchar(255),
+                              // FirstName varchar(255),
+                              // Address varchar(255),
+                              // City varchar(255))";
+                        $q1 = $_POST['gender'];
+                  #For adding column in a table $sql1 = "ALTER TABLE `test` ADD `extracolun` INT NOT NULL AFTER `City`"; #Code only for test
+                  #For creating foreign key link $sql2 = "ALTER TABLE `selected_option` ADD CONSTRAINT `user_linkto_option` FOREIGN KEY (`user_id`) REFERENCES `user_login_data`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; "
+                  $sql3 = "INSERT INTO selected_option($question[0]) VALUES('$q1')";
+                  $result = mysqli_query($conn,$sql3);
+                  if($result){
+                        echo "table created succesfully";
+                  }else{
+                        echo "some problem is there";
+                  }
             }
+      }
+		
+                  
+
+
+            
 
 
 ?>
@@ -15,19 +36,20 @@
 <!Doctype html>
 <html>
     <section class="container grey-text">
-		<h4 class="center">orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ?</h4>
-		<form class="white" action="portal.php" method="POST">
-            <input type="radio" id="male" name="gender" value="male">
-            <label for="male">Male</label><br>
-            <input type="radio" id="female" name="gender" value="female">
-            <label for="female">Female</label><br>
-            <input type="radio" id="other" name="gender" value="other">
-            <label for="other">Other</label> 
-            <div class="center">
-			<input type="submit" name="submit" value="Submit" class="btn brand ">
-		</div>
-			
-		</form>
+            <?php foreach($questions as $question) { ?>
+                  <h4 class="center"><?php echo $question[1] ?></h4>
+                  <form class="white" action="portal.php" method="POST">
+                  <input type="radio" id="male" name="gender" value="0">
+                  <label for="male"><?php echo $question[2] ?></label><br>
+                  <input type="radio" id="female" name="gender" value="1">
+                  <label for="female"><?php echo $question[3] ?></label><br>
+                  <input type="radio" id="other" name="gender" value="2">
+                  <label for="other"><?php echo $question[4] ?></label> 
+                  <div class="center">
+                        <input type="submit" name="<?php echo $question[0] ?>" value="Submit" class="btn brand ">
+                  </div>
+		<?php } ?>	
+            
 	</section>
 
 </html>
