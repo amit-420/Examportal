c90b6c35815f4f9968ff83e254ce49a13bca0f94
Prabuddha <prabuddhag1>
2020-12-29 11:30:05 +0530

    Link css file, add Bootstrap to rules.php

--- config/session_verification.php
+++ config/session_verification.php
@@ -9,13 +9,15 @@ $sql = "SELECT mem_email from user_login_data where mem_email = '$user_check'";
 $result = mysqli_query($conn, $sql);
 $userdata = mysqli_fetch_array($result);
 //
-print_r($userdata);
+
+// Commenting the next line out so that it doesn't display on the main page
+// print_r($userdata);
 //
 
 
 $count = mysqli_num_rows($result);
 if($result && $count == 1){
-    
+
 }else{
     // remove all session variables
     session_unset();
--- rules.php
+++ rules.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('config/db_connect.php');
 include('config/session_verification.php');
 if(isset($_POST['to_portal'])){
@@ -11,36 +11,51 @@ if(isset($_POST['to_portal'])){
 }
 ?>
 
-<!DOCTYPE HTML> 
-<html> 
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Instructions</title>
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- Google font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;800&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/style.css" />
+
+</head>
     <?php
     if($_SESSION['payment_status'] == True and $_SESSION['exam_status'] == false) {?>
-    <body> 
+    <body>
         <h4 class="center">Exam Rules</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Pellentesque vitae tempus velit, sit amet condimentum lacus. 
-        In et mi sit amet augue auctor cursus. Ut ultrices, risus 
-        sit amet fermentum volutpat, purus purus eleifend ex, id 
-        pharetra erat quam eget nibh. Ut facilisis eget felis 
-        condimentum imperdiet. Mauris varius, sapien eget posuere 
-        imperdiet, metus eros iaculis nunc, at lacinia ex augue non 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Pellentesque vitae tempus velit, sit amet condimentum lacus.
+        In et mi sit amet augue auctor cursus. Ut ultrices, risus
+        sit amet fermentum volutpat, purus purus eleifend ex, id
+        pharetra erat quam eget nibh. Ut facilisis eget felis
+        condimentum imperdiet. Mauris varius, sapien eget posuere
+        imperdiet, metus eros iaculis nunc, at lacinia ex augue non
         orci. Proin vitae lobortis eros, laoreet consectetur quam. Orci
-         varius natoque penatibus et magnis dis parturient montes, 
-         nascetur ridiculus mus. In quis hendrerit mi, nec feugiat 
-         sem. Donec a ipsum eu nibh venenatis commodo. Sed non 
+         varius natoque penatibus et magnis dis parturient montes,
+         nascetur ridiculus mus. In quis hendrerit mi, nec feugiat
+         sem. Donec a ipsum eu nibh venenatis commodo. Sed non
          interdum diam, vel scelerisque mi. </p>
          <form action="rules.php" method="POST">
          <input type="submit" name="to_portal" value="Start the Exam" >
          </form>
-    </body>  
+    </body>
     <?php } else if($_SESSION['payment_status'] == false and $_SESSION['exam_status'] == false){?>
-        <body> 
+        <body>
         <h4 class="center">Your payment is overdue.</h4>
         <p>Click the button below to pay the fee and resume with exam</p>
          <form action="rules.php" method="POST">
          <input type="submit" name="loginpage" value="Pay" >
          </form>
-    </body>  
+    </body>
     <?php } else if($_SESSION['payment_status'] == true and $_SESSION['exam_status'] == true){?>
         <h4 class="center">We appreciate your enthuciasm but You can give exam only once.</h4>
         <form action="rules.php" method="POST">
@@ -48,8 +63,10 @@ if(isset($_POST['to_portal'])){
         </form>
     <?php }?>
 
-            
-           
-  
 
-</html> 
+  <!-- Bootstrap javascript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</html>
